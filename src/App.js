ae06e5a9d2b8f3d75f3a1549fc1f12a8471c221f
Imran <imrantipu90@yahoo.com>
2022-09-28 10:08:46 +0200

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header/Header';
+
 
 function App() {
-  return 
-    
-  ;
+  return (
+    <div className="App">
+    <Header></Header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav>
+            <img src={logo} alt="" />
+        </nav>
+    );
+};
+
+export default Header;
